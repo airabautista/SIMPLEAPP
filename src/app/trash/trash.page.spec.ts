66966d51620c87bc42387553e055f6b23fe735c2
airabautista <bautista.aira05@gmail.com>
2023-06-07 23:21:25 +0800

    first commit

---
+++ src/app/trash/trash.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { TrashPage } from './trash.page';
+
+describe('TrashPage', () => {
+  let component: TrashPage;
+  let fixture: ComponentFixture<TrashPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(TrashPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
